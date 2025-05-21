f15a94e6a0f0483e16dfd8ddfbeb967bc3aca3d1
Agnieszka Ziomek <poziomkaa@gmail.com>
2025-05-21 16:30:13 +0200

    feat(app): initialize nuxt app

---
+++ README.md
@@ -0,0 +1,7 @@
+# Grocerace
+
+Shared shopping lists with real-time updates and points.
+
+## Purpose
+
+This app is build for portfolio purposes
---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <h1 class="m-1">Title <span class="i-material-symbols-add" /></h1>
+  </div>
+</template>
